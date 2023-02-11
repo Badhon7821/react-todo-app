ed15382ebc925b7ad8f18c151fb70a08d78bc89f
Badhon7821 <bayozidbostamibadhon@gmail.com>
2023-02-10 21:13:26 -0800

    done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,12 @@
 const Footer = () => {
   return (
-    <footer className="bg-gray-900 container mx-auto border-t border-dashed border-teal-300 p-10 text-teal-600  text-center text-md rounded-bl-xl rounded-br-xl">
-      <p>&copy; {new Date().getFullYear()} Todo app. All rights reserved.</p>
+    <footer className="bg-gray-900 container mx-auto border-t border-dashed border-teal-300 p-7 text-teal-600 text-md rounded-bl-xl rounded-br-xl">
+      <p className="text-center ">
+        &copy; {new Date().getFullYear()} Todo app. All rights reserved.
+      </p>
+      <p className="text-sm text-gray-500 py-2 font-semibold text-right">
+        Created by Badhon
+      </p>
     </footer>
   );
 };
